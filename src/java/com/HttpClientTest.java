36edd92696a72b04a344321dbb418d6292f85b7f
sumant <you@example.com>
2021-07-30 00:47:01 +0530

    tested HttpClient api

---
+++ src/java/com/HttpClientTest.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class HttpClientTest {
+    
+    public static void main(String [] args) throws IOException, InterruptedException{
+
+        HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_1_1)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+    HttpRequest request = HttpRequest.newBuilder()
+            .GET()
+            .uri(URI.create("https://api.publicapis.org/entries"))
+            .setHeader("User-Agent", "Java 11 HttpClient Bot")
+            .build();
+
+    HttpResponse<String> response =
+      httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+    HttpHeaders headers = response.headers();
+    headers.map().forEach((k, v) -> System.out.println(k + ":" + v));
+
+    System.out.println(response.statusCode());
+    System.out.println(response.body());
+    }
+}
